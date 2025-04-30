f1f78021512faca83c9fc5d6a581ca5e67f214e5
keremsenn <senkerem949@gmail.com>
2025-04-30 21:18:01 +0300

    app app module oluşturuldu

---
+++ client/app/src/main/java/com/keremsen/wordmasters/App.kt
@@ -0,0 +1,7 @@
+package com.keremsen.wordmasters
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App :Application()
---
+++ client/app/src/main/java/com/keremsen/wordmasters/AppModule.kt
@@ -0,0 +1,18 @@
+package com.keremsen.wordmasters
+
+import android.app.Application
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+    @Provides
+    fun provideContext(app: Application): Context {
+        return app.applicationContext
+    }
+}
